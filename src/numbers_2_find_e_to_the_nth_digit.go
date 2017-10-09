68b378cac5a1f84b2153e08dfaf9e73913e8f1df
seclu <grzegorz.imiolek@gmail.com>
2017-10-09 21:18:48 +0200

    Find e to the Nth Digit

---
+++ src/numbers_2_find_e_to_the_nth_digit.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	var n int
+	var decimalsCap int = 200
+
+	fmt.Println("Compute e how many decimal places (≤", decimalsCap, ")")
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if n > decimalsCap {
+		fmt.Println("Error:", n, "> 200 - too many decimal places!")
+		return
+	}
+
+	var format = []string{"%.", strconv.Itoa(n), "f"}
+	fmt.Printf(strings.Join(format, ""), math.E)
+}
